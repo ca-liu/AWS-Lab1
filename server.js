b5b308ffe743026b2f0df4a4b9b19a56e663f771
ca-liu <caleb.liu@outlook.com>
2022-01-05 15:20:10 -0800

    Add a get and post request

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 const express = require('express')
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello from the other side</h1>")
+app.use(express.json())
+
+const houses = [
+    {
+        id: 1,
+        address: {
+            streetNumber: 555,
+            streetName: "Seymour St",
+            city: "Vancouver",
+            province: "BC",
+            postalCode: "V6B 3H6"
+        },
+        listingPrice: 518446000,
+    }
+]
+
+app.get("/api/houses", (req, res) => {
+  res.send({houses:houses})
+});
+
+app.post("/api/houses", (req,res) => {
+    const data = req.body
+    data.id = houses.length+1
+    houses.push(data)
+    res.send(data)
 })
 
 const port = process.env.PORT || 8080
