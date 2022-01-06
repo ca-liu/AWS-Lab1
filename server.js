9d192853358a968cc6e47f5a075ad52e3d64a2a3
ca-liu <caleb.liu@outlook.com>
2022-01-05 23:31:25 -0800

    Add the react app

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+// Before the other routes
+app.use(express.static("build"))
+
+// the other resource routes
 app.use(express.json())
 
 const houses = [
@@ -27,6 +31,10 @@ app.post("/api/houses", (req,res) => {
     houses.push(data)
     res.send(data)
 })
+// After all other routes
+app.get('*', (req, res) => {
+    res.sendFile('build/index.html')});
 
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`listening on port ${port}`))
+
